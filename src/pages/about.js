518e20876b91fdb32b31cc1e6b057bdaaf73318c
Paweł Klimek <pawel.klimek@god-se.com>
2020-04-13 18:15:53 +0200

    create contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import "./about.scss"
 
 const AboutPage = () => {
     return (
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
+import "./contact.scss"
 
 const ContactPage = () => {
     return (
         <div>
             <Layout>
-Contact
+                <div className="background">
+                    <div className="card">
+                        <h1 className="header">KONTAKT</h1>
+                        <div className="line"/>
+                        <p className="contact">Serdecznie zapraszamy do kontaktu!</p>
+                        <p className="contact"> Zawsze chętnie udzielimy wszelakich informacji na temat naszych psów</p>
+                        <br/>
+                        <p className="contact">Założycielka hodowli: Karolina Klimek</p>
+                        <a className="contact" href="mailto:showfarm.kennel@gmail.com">showfarm.kennel@gmail.com</a>
+                        <p className="contact">+48 509 354 704</p>
+                        <a className="contact-link" href="https://www.facebook.com/showfarm.kennel">Facebook</a>
+                        <br/>
+                        <p className="contact">Poznań</p>
+                    </div>
+                </div>
             </Layout>
         </div>
     )
